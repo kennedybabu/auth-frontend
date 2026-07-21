4ab2457f131e04469471e527034c92c568bb354c
kennedybabu <kennedynjogu8@gmail.com>
2026-07-21 11:11:34 +0300

    add auth routes, styled the signup page

---
+++ src/app/components/shared/SignUp.interface.ts
@@ -0,0 +1,6 @@
+export class SignupUser {
+    "firstName": string
+    "lastName" : string
+    "email" : string
+    "password" : string
+}
--- src/app/pages/auth/signup/signup.ts
+++ src/app/pages/auth/signup/signup.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl,ReactiveFormsModule } from '@angular/forms'
+import { SignupUser } from '../../../components/shared/SignUp.interface';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
 export class Signup {
+    private formBuilder = inject(FormBuilder);
+
+    signupForm = this.formBuilder.group<SignupUser>({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    })
 
 }
