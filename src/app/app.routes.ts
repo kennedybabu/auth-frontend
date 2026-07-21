46faf927f10e74e3db30cfdf2e5e454f87e749e8
kennedybabu <kennedynjogu8@gmail.com>
2026-07-21 08:32:21 +0300

    add basic folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Signup } from './pages/auth/signup/signup';
+import { Home } from './pages/home/home';
+import { ForgotPassword } from './pages/auth/forgot-password/forgot-password';
+import { Login } from './pages/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'sign-up', component: Signup },
+    { path: 'reset-password', component:ForgotPassword },
+    { path: 'login', component: Login}
+];
